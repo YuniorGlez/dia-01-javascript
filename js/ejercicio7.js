73fa0f9f67cd8e209b7f439c8f6a5578171a2335
Leo_el21 <leonardowllst@gmail.com>
2025-03-31 21:11:39 +0200

    Ejercicio 7 retocado

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -6,7 +6,7 @@
 
 // Escribe tu código aquí debajo:
 
-let edad_usuario = prompt("Cual es tu edad?")
-if (edad_usuario > 18){
+let edad_usuario = prompt("Cual es tu edad?");
+if (edad_usuario >= 18){
     alert("Eres mayor de edad")
 }
